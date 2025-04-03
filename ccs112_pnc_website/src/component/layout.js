d0cced664afe08c62bebc48415843f80a9bd1883
Lawrence Mindanao <mindanaolawrence86@gmail.com>
2025-04-03 21:16:37 +0800

    minor change2

--- ccs112_pnc_website/src/component/layout.js
+++ ccs112_pnc_website/src/component/layout.js
@@ -11,17 +11,7 @@ const Layout = ({ children }) => {
 
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div className="container">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+         
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav mx-auto">
               <li className="nav-item">
